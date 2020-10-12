e6f3e05249ee013803f0a11cc969d8813e2aa91a
checky <checkywho@gmail.com>
2020-10-13 01:17:04 +0800

    fix build error in libace_kit_file

--- src/utils/native/lite/js/builtin/filekit/src/nativeapi_fs_impl.c
+++ src/utils/native/lite/js/builtin/filekit/src/nativeapi_fs_impl.c
@@ -1,29 +1,19 @@
 /*
- * Copyright (c) 2020 Huawei Device Co., Ltd.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright (c) 2020 FMSoft.
  */
 
 #include "nativeapi_fs_impl.h"
+
 #include <ctype.h>
 #include <dirent.h>
 #include <fcntl.h>
 #include <limits.h>
-#include <securec.h>
 #include <stdbool.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include "nativeapi_config.h"
+
+#include "securec.h"
 
 #define BUFFER_SIZE 512
 
