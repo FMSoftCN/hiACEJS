747dfd2299100997eee9ccffc980978f96241319
Checky-Hu <checkywho@gmail.com>
2020-11-06 15:06:18 +0800

    refactor files in test dir

--- test/src/main.c
+++ test/src/main.c
@@ -5,6 +5,6 @@
 int main(int argc, char ** argv)
 {
     HiAceJs hi_ace_js;
-    HiAceJsRun(NULL, NULL, &hi_ace_js);
+    HiAceJsRun("com.huawei.airquality", "airquality", &hi_ace_js);
     return 0;
 }
