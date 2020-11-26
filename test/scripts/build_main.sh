5b5bfcd914f07ce455b63778da262df99483b75d
xueshuming <xueshuming@minigui.org>
2020-11-26 20:14:01 +0800

    update build_main

--- test/scripts/build_main.sh
+++ test/scripts/build_main.sh
@@ -4,8 +4,4 @@ if [ ! -d build ];then
   mkdir build
 fi
 
-gcc -Wall -g src/main.c -I../src/foundation/ace/frameworks/lite/targets/hybridos -L../src/out/hybridos \
-  -lmbedtls -lcipher_shared -lcjson_shared -lsec_shared -lgraphic_hals -lgraphic_utils -lui_shared \
-  -ljerry-core_shared -ljerry-ext_shared -ljerry-libm_shared -ljerry-port-default_shared \
-  -lace_kit_cipher -lhilog_shared -lace_kit_timer -lace_kit_common -lace_kit_deviceinfo \
-  -lace_kit_file -lace_kit_kvstore -lkal_timer -lability_shared -lace_lite -o build/main
+gcc -Wall -g src/main.c -I../src/foundation/ace/frameworks/lite/targets/hybridos  -Wl,-rpath-link ../output/root/usr/lib/  -L../output/root/usr/lib -lace -o build/main
