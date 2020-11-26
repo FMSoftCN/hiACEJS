02d2e696bcc0da8b499b302fd1bb48f78a7221a5
xueshuming <xueshuming@minigui.org>
2020-11-26 18:24:36 +0800

    add build/scripts/build_third_party.sh

---
+++ build/scripts/build_third_party.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+BASE_DIR=$(dirname $(readlink -f "$0"))
+
+# output dir
+OUTPUT_DIR=$BASE_DIR/../../output
+ROOT_DIR=$OUTPUT_DIR/root
+mkdir -p $ROOT_DIR
+
+# build jerryscipt
+pushd ${BASE_DIR}/../../src/third_party/jerryscript
+
+python tools/build.py --mem-heap=48 --snapshot-exec=ON --snapshot-save=ON \
+        --profile=es5.1 --error-messages=ON --logging=ON --mem-stats=ON \
+        --jerry-cmdline-snapshot=ON --compile-flag="-Wno-unused-function" \
+        --install=$ROOT_DIR/usr
+
+popd
