48f80104640bb513ed7ac8f9a003818917d307b8
xueshuming <xueshuming@minigui.org>
2020-11-27 08:57:17 +0800

    modify FindMbedTLS.cmake

--- cmake/FindMbedTLS.cmake
+++ cmake/FindMbedTLS.cmake
@@ -1,8 +1,9 @@
 
-find_path(MBEDTLS_INCLUDE_DIRS mbedtls/ssl.h)
+find_path(MBEDTLS_INCLUDE_DIR mbedtls/ssl.h)
 find_library(MBEDTLS_LIBRARY mbedtls)
 find_library(MBEDX509_LIBRARY mbedx509)
 find_library(MBEDCRYPTO_LIBRARY mbedcrypto)
+set(MBEDTLS_INCLUDE_DIRS "${MBEDTLS_INCLUDE_DIR}/mbedtls")
 set(MBEDTLS_LIBRARIES "${MBEDTLS_LIBRARY}" "${MBEDX509_LIBRARY}" "${MBEDCRYPTO_LIBRARY}")
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(MBEDTLS DEFAULT_MSG
