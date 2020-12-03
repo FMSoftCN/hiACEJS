e27c272374ec6a32bb990d28521beecf9122c2e3
xueshuming <xueshuming@minigui.org>
2020-12-03 16:39:44 +0800

    impl HybridosSurface::Lock

--- src/foundation/ace/frameworks/lite/targets/hybridos/hybridos_surface.cpp
+++ src/foundation/ace/frameworks/lite/targets/hybridos/hybridos_surface.cpp
@@ -13,10 +13,16 @@ HybridosSurface::~HybridosSurface()
 
 void HybridosSurface::Lock(void** buf, void** phyMem, uint32_t* strideLen)
 {
+    int width = 0;
+    int height = 0;
+    int pitch = 0;
+    *strideLen = GetGDCapability (m_hdc, GDCAP_BPP);
+    *buf = LockDC (m_hdc, m_rect, &width, &height, &pitch);
 }
 
 void HybridosSurface::Unlock()
 {
+    UnlockDC(m_hdc);
 }
 
 }
