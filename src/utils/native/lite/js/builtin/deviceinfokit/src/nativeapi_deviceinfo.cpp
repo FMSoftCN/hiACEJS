6533b60b0e510bb3458f62b7492c225a8f267e53
xueshuming <xueshuming@minigui.org>
2020-12-18 13:06:51 +0800

    impl device.getInfo

--- src/utils/native/lite/js/builtin/deviceinfokit/src/nativeapi_deviceinfo.cpp
+++ src/utils/native/lite/js/builtin/deviceinfokit/src/nativeapi_deviceinfo.cpp
@@ -1,12 +1,70 @@
+/////////////////////////////////////////////////////////////////////////////// //
+//                          IMPORTANT NOTICE
+//
+// The following open source license statement does not apply to any
+// entity in the Exception List published by FMSoft.
+//
+// For more information, please visit:
+//
+// https://www.fmsoft.cn/exception-list
+//
+//////////////////////////////////////////////////////////////////////////////
+/**
+ \verbatim
+
+    This file is part of HybridOS, a developing operating system based on
+    MiniGUI. HybridOs will support embedded systems and smart IoT devices.
+
+    Copyright (C) 2020 Beijing FMSoft Technologies Co., Ltd.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Or,
+
+    As this program is a library, any link to this program must follow
+    GNU General Public License version 3 (GPLv3). If you cannot accept
+    GPLv3, you need to be licensed from FMSoft.
+
+    If you have got a commercial license of this program, please use it
+    under the terms and conditions of the commercial license.
+
+    For more information about the commercial license, please refer to
+    <http://www.minigui.com/blog/minigui-licensing-policy/>.
+
+ \endverbatim
+ */
+
 /*
- * Copyright (c) 2020 FMSoft.
+ * Copyright (c) 2020 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 #include "nativeapi_deviceinfo.h"
-
 #include "js_async_work.h"
 #include "nativeapi_common.h"
 #include "nativeapi_config.h"
+#include "parameter.h"
 #include "screen.h"
 
 namespace OHOS {
@@ -38,14 +96,14 @@ void ExecuteGetInfo(void* data)
     }
     JSIValue args = params->args;
     JSIValue thisVal = params->thisVal;
-    char* brand = (char*)"Hybridos";
+    char* brand = GetBrand();
     if (brand == nullptr) {
         NativeapiCommon::FailCallBack(args, thisVal, ERROR_CODE_GENERAL);
         JSI::ReleaseValueList(args, thisVal, ARGS_END);
         delete params;
         return;
     }
-    char* manufacture = (char*)"FMSoft";
+    char* manufacture = GetManufacture();
     if (manufacture == nullptr) {
         free(brand);
         NativeapiCommon::FailCallBack(args, thisVal, ERROR_CODE_GENERAL);
@@ -53,7 +111,7 @@ void ExecuteGetInfo(void* data)
         delete params;
         return;
     }
-    char* model = (char*)"hiACEJS";
+    char* model = GetProductModel();
     if (model == nullptr) {
         free(brand);
         free(manufacture);
